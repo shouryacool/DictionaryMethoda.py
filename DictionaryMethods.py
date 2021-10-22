a7b381802b45d149e0f64770cd4815eaa28dfc18
shouryacool <84572733+shouryacool@users.noreply.github.com>
2021-10-22 23:18:44 +0530

    Add files via upload

---
+++ DictionaryMethods.py
@@ -0,0 +1,23 @@
+d4={"1":"Hi","5":"Harry","6":"Subha,"}
+print(d4.keys())     #Print The Keys Of The Dictionary    
+print(d4.values())    #Print The Value Of The Dictionary
+print(d4.items())     #Print The (Keys,Values) For All Contents Of The Dictionary
+
+updated4={
+    "9":"Hello"
+}  
+d4.update(updated4)    #Update The Dictionary by adding key value  pairs from Dictionary
+print(d4.get("7"))    #This Will Return None 
+print(d4["7"])       #This will Return  A error As Key "7" Is Not Present In the dictionary
+
+
+
+
+
+
+
+
+
+
+
+
